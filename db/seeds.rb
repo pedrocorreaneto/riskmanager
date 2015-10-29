1f29bbe9a09ed200fc1a986b8bec12cd980ee981
Pedro Corrêa <pedro@Blackbird>
2015-10-28 22:10:29 -0300

    Resolução de um pequeno bug e arrumação do seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,7 @@ Project.create!([
   {title: "Site Simples", description: "Site simples para demonstração de uma ideia"},
   {title: "Conta de e-mail", description: "Portal com e-mail"},
   {title: "Aparelho de celular", description: "Aparelho smartfone"},
-  {title: "Conta bancária", description: "site de internet banking"}
-])
-Requirement.create!([
-  {project_id: 2, threat_id: 1, solution_id: 1, riskaccept: false},
-  {project_id: 3, threat_id: 1, solution_id: 2, riskaccept: false},
-  {project_id: 4, threat_id: 1, solution_id: 3, riskaccept: false},
-  {project_id: 1, threat_id: 1, solution_id: 1, riskaccept: true}
-])
-Solution.create!([
-  {title: "Resolução de Um Captcha", description: "Após um número finito de tentativas mal sucedidas, o sistema solicitará um captcha para permitir acesso", cons: "Necessita de acesso a dados externos, para obtenção do captcha", usedtimes: nil, threat_id: 1},
-  {title: "Temporizador", description: "Após um número finito de tentativas, o sistema irá impedir uma nova tentativa por um tempo determinado. ", cons: "O usuário legitimo pode ficar sem acesso ao sistema por muito tempo.", usedtimes: nil, threat_id: 1},
-  {title: "Bloqueio de conta", description: "Após um número finito de tentativas a conta do usuário pode ser bloqueada", cons: "Se faz necessário uma terceira parte, com autoridade sobre o sistema, para desbloqueio.", usedtimes: nil, threat_id: 1},
-  {title: "Armazenar Hash", description: "Armazenar o hash da senha, e não a senha em texto plano.", cons: "O sistema terá que refazer o calculo do hash da senha toda vez que o usuário logar", usedtimes: nil, threat_id: 4}
+  {title: "Conta bancária", description: "Site de internet banking"}
 ])
 Threat.create!([
   {title: "Ataque de força bruta", category: "s", description: "Realização de um ataque de força bruta a um sistema de autenticação."},
@@ -22,4 +10,15 @@ Threat.create!([
   {title: "Captura de Sessão", category: "s", description: "A partir da monitoração de trafego, o atacante pode ser capaz de ter acesso a sessão do usuário"},
   {title: "Descoberta de senhas do usuário", category: "i", description: "A partir de consulta ao banco de dados, o atacante pode descobrir a senha de todos os usuários"},
   {title: "Alteração de senha involuntariamente", category: "r", description: "a partir de uma sessão logada, o atacante pode ser capaz de alterar a senha do usuário"}
+])
+Solution.create!([
+  {title: "Resolução de Um Captcha", description: "Após um número finito de tentativas mal sucedidas, o sistema solicitará um captcha para permitir acesso", cons: "Necessita de acesso a dados externos, para obtenção do captcha", usedtimes: nil, threat_id: 1},
+  {title: "Temporizador", description: "Após um número finito de tentativas, o sistema irá impedir uma nova tentativa por um tempo determinado", cons: "O usuário legitimo pode ficar sem acesso ao sistema por muito tempo.", usedtimes: nil, threat_id: 1},
+  {title: "Bloqueio de conta", description: "Após um número finito de tentativas a conta do usuário pode ser bloqueada", cons: "Se faz necessário uma terceira parte, com autoridade sobre o sistema, para desbloqueio.", usedtimes: nil, threat_id: 1}
+])
+Requirement.create!([
+  {project_id: 1, threat_id: 1, solution_id: nil, riskaccept: true},
+  {project_id: 2, threat_id: 1, solution_id: 1, riskaccept: false},
+  {project_id: 3, threat_id: 1, solution_id: 2, riskaccept: false},
+  {project_id: 4, threat_id: 1, solution_id: 3, riskaccept: false}
 ])
