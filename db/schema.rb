2bd92aed6abe47eead4a0b9aefaa5a9706ee4c5e
Pedro Corrêa <pedro@Blackbird>
2015-12-02 18:33:10 -0300

    Alterado o nome de Cons para consequences

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151117002449) do
+ActiveRecord::Schema.define(version: 20151202211848) do
 
   create_table "mitigations", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.text     "cons"
+    t.text     "consequences"
     t.integer  "usedtimes"
     t.integer  "threat_id"
     t.datetime "created_at"
